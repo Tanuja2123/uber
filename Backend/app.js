d0ad07ad8caf2f9b43108b21e7da5bf43d4e1b08
Tanuja Sharma <tanujas605@gmail.com>
2025-01-15 11:47:24 +0530

    user authentication done

--- Backend/app.js
+++ Backend/app.js
@@ -6,10 +6,12 @@ const cors=require('cors');
 const app = express();
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
+const cookieParser = require('cookie-parser');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
 
 connectToDb();
 
